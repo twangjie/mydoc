42f323e4767e4a5969d71f4e6329d598bc325df1
twangjie <twangjie@qq.com>
2017-09-14 23:36:58 +0800

    update kcpdante

--- docker/kcpdante/entrypoint.sh
+++ docker/kcpdante/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 /usr/sbin/sockd -D
-kcptun-server -l :21000 -t 127.0.0.1:1080 --key twangjie --crypt xtea --mode fast --mtu 1350 --sndwnd 1024 --rcvwnd 1024
+#kcptun-server -l :21000 -t 127.0.0.1:1080 --key test --crypt aes --mode fast --mtu 1350 --sndwnd 1024 --rcvwnd 1024
+kcptun-server -c /etc/kcptun/kcptun-socks5.json
